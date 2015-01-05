da9e8c6efd8bbb6e188fb2966e86fc625bddb64e
Niko Wenselowski <der@nik0.de>
2015-01-05 22:45:47 +0100

    pep8.

--- obsversioncheck.py
+++ obsversioncheck.py
@@ -60,12 +60,14 @@ def show_project(project, os=None):
                            software=software,
                            important_software=IMPORTANT_SOFTWARE)
 
+
 @app.route("/")
 def show_overview():
     if not PROJECTS_TO_CHECK:
         return "No projects configured."
 
-    projects = {project: get_operating_systems(project) for project in PROJECTS_TO_CHECK}
+    projects = {project: get_operating_systems(project)
+                for project in PROJECTS_TO_CHECK}
 
     return render_template('overview.html', projects=projects)
 
@@ -102,7 +104,8 @@ def parse_repository(link_to_repository):
     print("Parsing repo: {0}".format(link_to_repository))
 
     try:
-        return {split_name_and_version(sw)[0]: split_name_and_version(sw)[1] for sw in get_software_from_repository(link_to_repository)}
+        return {split_name_and_version(sw)[0]: split_name_and_version(sw)[1]
+                for sw in get_software_from_repository(link_to_repository)}
     except Exception as err:
         print("Fuck: {0}".format(err))
         return {}
@@ -130,7 +133,8 @@ def split_name_and_version(filename):
     if match:
         return (match.group('name'), match.group('version'))
 
-    raise ValueError("Unable to split software name and version from {0}".format(filename))
+    raise ValueError("Unable to split software name and "
+                     "version from {0}".format(filename))
 
 
 if __name__ == "__main__":
